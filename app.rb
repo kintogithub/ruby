31ce7620f50192b7e53cf3c986814be67cf247d2
Nadeem Khedr <nadeemkhedr@gmail.com>
2018-06-15 15:51:40 -0700

    <2728> feat(general): update endpoint and make it simpler

--- app.rb
+++ app.rb
@@ -5,18 +5,15 @@ set :port, (ENV["PORT"] or 8000)
 set :bind, '0.0.0.0'
 
 =begin
-@api {get} /sample/{message} hello world sample request
-@apiName GetSample
-@apiParam (Url) {String} message the message to return
-@apiSuccess (Success_200) {String} data the hello world data
-@apiSuccess (Success_200) {String} output what the user entered in the url
+@api {get} /hello/{name} Prints "Hello {name}"
+@apiName HelloWorld
+@apiParam (Url) {String} name the name to print
+@apiSuccess (200) {String} message the hello {name} message
 =end
-
-get '/sample/:message' do
+get '/hello/:name' do
   content_type :json
   {
-    data: "Hello World",
-    output: params['message']
+    message: "Hello #{params['name']}"
   }.to_json
 end
 
